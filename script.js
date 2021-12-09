7303eb5b13b615da971e4d1116575a637c02fdc3
dberkshire <dberkshireee@gmail.com>
2021-12-09 08:39:50 -0800

    some js added

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const bingoLetters = ['B', 'I', 'N', 'G', 'O'];
+
 const bingoChoices = [];
+
 const USER = {
-	playerOne: 'green'
+	playerOne: 'green',
 };
-const WINNING_COMBOS = [[0,1,2,3,4], [5,6,7,8,9], [10,11,12,13,14], [15,16,17,18,19], [20,21,22,23,24], [0,6,12,18,24], [4,8,12,16,20], [0,5,10,15,20], [1,6,11,16,21], [2,7,12,17,22], [3,8,13,18,23], [4,9,14,19,24]];
 
+const WINNING_COMBOS = [
+	[0, 1, 2, 3, 4],
+	[5, 6, 7, 8, 9],
+	[10, 11, 12, 13, 14],
+	[15, 16, 17, 18, 19],
+	[20, 21, 22, 23, 24],
+	[0, 6, 12, 18, 24],
+	[4, 8, 12, 16, 20],
+	[0, 5, 10, 15, 20],
+	[1, 6, 11, 16, 21],
+	[2, 7, 12, 17, 22],
+	[3, 8, 13, 18, 23],
+	[4, 9, 14, 19, 24],
+];
 
+let winning;
+let ticMarks;
 
+const bingoCard = document.querySelector('#bingo-board');
+const playAgainBtn = document.querySelector('button');
+const currentValue = document.querySelector('h2');
+
+bingoCard.addEventListener('click', handleClick);
+playAgainBtn.addEventListener('click', init);
 
 bingoLetters.forEach((letter) => {
 	for (let i = 0; i < 76; i++) {
@@ -14,3 +37,7 @@ bingoLetters.forEach((letter) => {
 		// The letter B should be numbers 1 - 15, I should be numbers 16 - 30, N should be 31 - 45, G should be 46 - 60, O should be 61 - 75
 	}
 });
+
+function init() {}
+
+function handleClick(event) {}
