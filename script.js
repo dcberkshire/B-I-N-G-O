12add82f499f3e4353a49599e14bc17a074f72e3
dberkshire <dberkshireee@gmail.com>
2021-12-10 12:55:24 -0800

    more js added

--- script.js
+++ script.js
@@ -21,8 +21,10 @@ const WINNING_COMBOS = [
 	[4, 9, 14, 19, 24],
 ];
 
+let currentUser;
 let winning;
 let ticMarks;
+let makeBoard;
 
 const bingoCard = document.querySelector('#bingo-board');
 const playAgainBtn = document.querySelector('button');
@@ -32,12 +34,59 @@ bingoCard.addEventListener('click', handleClick);
 playAgainBtn.addEventListener('click', init);
 
 bingoLetters.forEach((letter) => {
-	for (let i = 0; i < 76; i++) {
-		bingoChoices.push(`${letter}${i}`);
+	for (let i = 0; i < 75; i++) {
+		if (i <= 15) {
+			bingoChoices.push(`${letter}${i}`)
+		} if (i <= 30) {
+			bingoChoices.push(`${letter}${i}`)
+		} if (i <= 45) {
+			bingoChoices.push(`${letter}${i}`)
+		} else if (i <= 60) {
+			bingoChoices.push(`${letter}${i}`)
+		} else (i <= 75) 
+			bingoChoices.push(`${letter}${i}`)
+		};
+		// bingoChoices.push(`${letter}${i}`);
 		// The letter B should be numbers 1 - 15, I should be numbers 16 - 30, N should be 31 - 45, G should be 46 - 60, O should be 61 - 75
-	}
 });
 
-function init() {}
+function init() {
+	currentUser = USER.playerOne;
+	winning = false;
+	makeBoard = new Array(25).fill(bingoLetters);
+	ticMarks = new Array(25).fill(null);
+	create();
+};
 
-function handleClick(event) {}
+init();
+
+function handleClick(event) {
+	const squareIndex = event.target.id;
+	if (ticMarks[squareIndex]) return;
+	if (winning) return;
+	ticMarks[squareIndex] = currentUser;
+	checkWinner();
+	create();
+};
+
+function create(){
+	for (let i = 0; i <ticMarks.length; i++){
+		const square = document.getElementById(i);
+		square.style.backgroundColor = ticMarks[i];
+	};
+
+};
+
+function checkWinner() {
+	for (let i = 0; i < WINNING_COMBOS.length; i++){
+		const squareOne = ticMarks[WINNING_COMBOS[i][0]];
+		const squareTwo = ticMarks[WINNING_COMBOS[i][1]];
+		const squareThree = ticMarks[WINNING_COMBOS[i][2]]
+		const squareFour = ticMarks[WINNING_COMBOS[i][3]];
+		const squareFive = ticMarks[WINNING_COMBOS[i][4]];
+
+		if (squareOne && squareOne === squareTwo && squareTwo === squareThree && squareThree === squareFour && squareFour === squareFive){
+			winning = squareOne;
+		};
+	};
+};
